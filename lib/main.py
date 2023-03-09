552b4c941b0ebec1a6f538168c1c9a40cef0fe3a
Mayowa Ajiboye <64916584+mayowa-aji@users.noreply.github.com>
2023-03-09 16:04:39 -0500

    add loop, add function to run app, add more descriptive text

--- lib/main.py
+++ lib/main.py
@@ -7,6 +7,21 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+# commands = ["add" ,"delete","show"]
+commands = ["""
+**********************************************************************************
+
+        "add -[Hint] - Use underscore instead of spaces between words"
+
+        "delete -[Hint] - Use the index of the task you would like to delete"
+
+        "display -[Hint] - Displays all tasks"
+
+**********************************************************************************
+  """]
+CONTEXT_SETTINGS = dict(help_options_names= ['-h', '--help'])
+
+
 class Task(Base):
     __tablename__ = 'tasks'
 
@@ -17,18 +32,30 @@ class Task(Base):
     def __repr__(self):
         return f'Task(id={self.id}, task_name="{self.task_name}", priority="{self.priority}")'
 
+class QuitHandler:
+    def __init__(self,is_quit=False):
+        self._is_quit = is_quit
+    @property
+    def is_quit(self):
+        return self._is_quit
+    @is_quit.setter
+    def is_quit(self, value):
+        self._is_quit = value
+
 engine = create_engine('sqlite:///todo.db', echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
+quithandler = QuitHandler()
 
 Base.metadata.create_all(engine)
 
 os.system('cls' if os.name=='nt' else 'clear')
+
 @click.group()
+# @click.command(context_settings = CONTEXT_SETTINGS)
 def cli():
     pass
 
-@click.command()
 def welcome():
     message = '''
 
@@ -40,8 +67,14 @@ def welcome():
    _/ /  '. `---' .`'. `--' .`    '.   \) \ '. `---' .` )/          )/  -'    `-(_/\_),-'   |
   (_.'     `-...-'    `-..-'        `-.(_.'   `-...-'   (           (                 (_..--'
 
+
+   Welcome to YOU GOT THIS!! An application that allows you to organize your day by creating, listing and deleting completed tasks. ENJOY!!
+
     '''
     click.echo(Fore.CYAN + Back.BLACK+ message)
+    click.echo("Please select one of the below commands to proceed:")
+    for cmd in commands:
+        click.echo(cmd)
 
 @click.command()
 @click.argument('task')
@@ -54,6 +87,9 @@ def add(task, priority, priority_levels):
         'medium': {1: 'medium', 2: 'high', 3: 'urgent'},
         'high': {1: 'high', 2: 'urgent', 3: 'critical'}
     }
+    task = task.split("_")
+    task = " ".join(task)
+    print(task)
     priority_level = priority_dict[priority_levels][priority]
     new_task = Task(task_name=task, priority=priority_level)
     session.add(new_task)
@@ -74,16 +110,37 @@ def delete(task_id):
 
 @click.command()
 @click.option('-p', '--priority', type=click.Choice(['tuple', 'list', 'dict']), default='tuple', help='Type of priority list')
-def show(priority):
+def display(priority):
     """Let me see what I have to do """
     tasks = session.query(Task).order_by(Task.priority).all()
     for task in tasks:
         click.echo(Fore.BLUE + Back.BLACK + f'Priority: [{task.priority}]  {task.task_name}')
 
-cli.add_command(welcome)
+
+
+@click.command()
+# @click.option('--run', help = "Runs the programme")
+def run():
+    welcome()
+    while quithandler.is_quit == False:
+      cmd = click.prompt("Please enter a command to proceed",  type=str)
+      split_text = cmd.split()
+      try:
+          cli(split_text)
+      except SystemExit:
+          pass
+
+
+@click.command()
+def quit():
+    quithandler.is_quit = True
+    os.system('cls' if os.name=='nt' else 'clear')
+    click.echo("Have a great day!")
+
 cli.add_command(add)
 cli.add_command(delete)
-cli.add_command(show)
+cli.add_command(display)
+cli.add_command(quit)
 
 if __name__ == '__main__':
-    cli()
+    run()
